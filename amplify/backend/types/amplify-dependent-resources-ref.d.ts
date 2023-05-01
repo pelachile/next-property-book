534aaa6909f133995355aaa4913ff369d672eadd
Patrick Moore <cory@corymoore.com>
2023-05-01 16:45:57 -0500

    deleted amplify to start over

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "propertybook": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
